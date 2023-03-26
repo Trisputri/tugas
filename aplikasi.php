99240cf88b1efba49f401f4c26ad9b99e6e73365
trisputri <trisputri564@gmal.com>
2023-03-26 21:23:36 +0700

    first commit

---
+++ aplikasi.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+    <title>perhitungan matematika</title>
+</head>
+
+<body>
+    <h5> APLIKASI PERHITUNGAN MATEMATIKA </h5>
+    <form action="proses.php" method="POST">
+        Bilangan 1 : <br>
+        <input type="text" name="txtbil1"><br>
+        Bilangan 2 : <br>
+        <input type="text" name="txtbil2"><br>
+        <button type="submit">TAMBAH</button>
+    </form>
+</body>
+
+<body>
+<form action="proses.php" method="POST">
+        Bilangan 1 : <br>
+        <input type="text" name="txtbil1"><br>
+        Bilangan 2 : <br>
+        <input type="text" name="txtbil2"><br>
+        <button type="submit">KURANG</button>
+    </form>
+
+</body>
+
+<body>
+<form action="proses.php" method="POST">
+        Bilangan 1 : <br>
+        <input type="text" name="txtbil1"><br>
+        Bilangan 2 : <br>
+        <input type="text" name="txtbil2"><br>
+        <button type="submit">KALI</button>
+    </form>
+
+</body>
+
+<body>
+<form action="proses.php" method="POST">
+        Bilangan 1 : <br>
+        <input type="text" name="txtbil1"><br>
+        Bilangan 2 : <br>
+        <input type="text" name="txtbil2"><br>
+        <button type="submit">BAGI</button>
+    </form>
+
+</body>
+</html>
